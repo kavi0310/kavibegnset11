aa2d4eb1da93acb351254c238adc01a314a65af4
kavi0310 <46838903+kavi0310@users.noreply.github.com>
2019-02-12 00:39:23 +0530

    Create 117.c

---
+++ 117.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int a[10],n1,n2,i,m,k,c,p;
+scanf("%d%d",&n1,&n2);
+m=n1-n2;
+for(i=0;i<n1;i++)
+{
+scanf("%d",&a[i]);
+}
+for(k=0;k<=m;k++)
+{
+c=0;
+for(i=0;i<n1;i++)
+{
+if(a[i]>c)
+{
+c=a[i];
+p=i;
+}
+}
+a[p]=0;
+}
+printf("%d",c);
+return 0;
+}
+
